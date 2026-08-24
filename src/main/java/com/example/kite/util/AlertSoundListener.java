dac6f8512cb5eda7458ecc59d264aa4db4476fcf
rajireddy kesireddy <rajreddy.kesireddy@tailoredbrands.com>
2026-08-24 16:31:49 +0530

    enabled sound for websocket

--- src/main/java/com/example/kite/util/AlertSoundListener.java
+++ src/main/java/com/example/kite/util/AlertSoundListener.java
@@ -12,9 +12,14 @@ public class AlertSoundListener {
 
     public static void beep(SignalSourceType source) {
         String environment = System.getProperty("env");
+        String dir = System.getProperty("file.dir.path");
+        if(dir == null) {
+            dir = "C:\\Windows\\Media\\";
+        }
         if("local".equals(environment)) {
             try {
-                String filePath = soundFilePath(source);
+                String filePath = soundFilePath(source, dir);
+                log.info("sound file path: {}", filePath);
                 File soundFile = new File(filePath);
                 //File soundFile = new File("beep.mp3");
                 try (AudioInputStream audioIn =
@@ -37,13 +42,13 @@ public class AlertSoundListener {
         }
     }
 
-    private static String soundFilePath(SignalSourceType source) {
-
+    private static String soundFilePath(SignalSourceType source, String filePath) {
+        log.info("Signal source: {} and sound file source: {}", source, filePath);
         return switch (source) {
-            case SignalSourceType.WEBHOOK: yield "C:\\Windows\\Media\\Ring01.wav";
-            case SignalSourceType.emaCrossoverStrategy: yield "C:\\Windows\\Media\\Ring08.wav";
-            case SignalSourceType.IntradayTradingEngine: yield "C:\\Windows\\Media\\Ring04.wav";
-            case SignalSourceType.IntradayTradingEngineV2: yield "C:\\Windows\\Media\\Alarm08.wav";
+            case SignalSourceType.WEBHOOK: yield filePath+"Ring01.wav";
+            case SignalSourceType.emaCrossoverStrategy: yield filePath+"Ring08.wav";
+            case SignalSourceType.IntradayTradingEngine: yield filePath+"Ring04.wav";
+            case SignalSourceType.IntradayTradingEngineV2: yield filePath+"Alarm08.wav";
         };
     }
 }
